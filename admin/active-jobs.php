a2c6ac9baaa431a387693d2530ac655d81ca7324
Tanmay9223 <102200932+Tanmay9223@users.noreply.github.com>
2023-11-23 01:18:31 +0530

    Update active-jobs.php

--- admin/active-jobs.php
+++ admin/active-jobs.php
@@ -176,7 +176,6 @@ require_once("../db.php");
     <div class="control-sidebar-bg"></div>
 
   </div>
-  <!-- ./wrapper -->
 
   <!-- jQuery 3 -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
